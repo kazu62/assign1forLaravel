c4f900fce72c50d6046aadd0046958edd5c6ca1a
KazuhisaOno <k.a-z.ono@betelgeuse-corp.com>
2019-05-02 15:59:36 +0900

    修正お

--- app/Http/Controllers/MstPrefectureController.php
+++ app/Http/Controllers/MstPrefectureController.php
@@ -10,8 +10,7 @@ use App\Http\Resources\Mst_prefecture as Mst_prefectureResource;
 class MstPrefectureController extends Controller
 {
     public function index(){
-        $prefectures=Mst_prefecture::orederBy('prefecture_cd','asc')
-        ->paginate(10);
+        $prefectures=Mst_prefecture::paginate(10);
         return Mst_prefectureResource::collection($prefectures);
     }
 
