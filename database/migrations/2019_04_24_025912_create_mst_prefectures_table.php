6577652721492bac59107b49c51b9974d556d910
KazuhisaOno <k.a-z.ono@betelgeuse-corp.com>
2019-05-02 14:19:01 +0900

    migration修正

--- database/migrations/2019_04_24_025912_create_mst_prefectures_table.php
+++ database/migrations/2019_04_24_025912_create_mst_prefectures_table.php
@@ -14,7 +14,7 @@ class CreateMstPrefecturesTable extends Migration
     public function up()
     {
         Schema::create('mst_prefectures', function (Blueprint $table) {
-            $table->interger('id');
+            $table->integer('id');
             $table->char('prefecture_cd',2);
             $table->primary('prefecture_cd');
             $table->string('prefecture_name',20);
