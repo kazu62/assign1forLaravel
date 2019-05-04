9aa208c75700eefcd18937fe787b8b71d690be5e
KazuhisaOno <k.a-z.ono@betelgeuse-corp.com>
2019-05-04 18:11:31 +0900

    修正

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-
-Route::match(['options','post'],'/mst_prefecres/search', 'MstPrefectureController@index');
-Route::match(['options','post'],'/mst_prefecres', 'MstPrefectureController@store');
